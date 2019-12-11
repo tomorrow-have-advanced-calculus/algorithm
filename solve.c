91461dee4972934464b6717dfc02ee232d3841d1
ZoneTwelve <zonetwelve159@gmail.com>
2019-12-11 19:06:46 +0800

    [add] C/C++ version

---
+++ solve.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int power(int a, int n){
+  int t = 1;
+  for(n;n>0;n--)
+    t*=a;
+  return t;
+}
+
+int recursive(int c, int d, int n){
+  int m = c, s = 1;
+  while(m<n){
+    if(s==d)
+      return m%n;
+    m*=c;
+    s++;
+  }
+  int a = d%s,
+      b = d/s;
+  return ( power(c, a) * recursive( m%n, b, n ) )%n;
+}
+
+int main(int argc, int *argv[]){
+  int p = 11, q = 37;
+  int n = 407, phin = 360;
+  int e = 7, d = 103;
+  int message = 41, ciphertext = 178;
+
+  int C = ciphertext, D = d, N = n;
+  
+  printf("%d\n", recursive(C, D, N));
+}
