dbd54cb9343f1df1be5ea80f640a1a4f04aa360a
ZoneTwelve <zonetwelve159@gmail.com>
2019-12-11 18:53:14 +0800

    [add] first version (JavaScript)

---
+++ solve.js
@@ -0,0 +1,19 @@
+let p = 11, q = 37;
+let c = parseInt(process.argv[2])||41,  //0x41 = 'A' 
+    d = parseInt(process.argv[3])||(7), //e = 103 
+    n = parseInt(process.argv[4])||(p*q);
+function recursive(c, d, n){
+  let m = c, s = 1;
+  while(m<n){
+    if(s==d)
+      return m%n;
+    m*=c; //mul c
+    s+=1; //inc s
+  }
+  let k = d;
+  let a =    k % s;
+  let b = ~~(k / s);
+  return ( Math.pow(c, a) * recursive( m%n, b, n ) ) % n;
+}
+
+console.log(recursive(c, d, n));
